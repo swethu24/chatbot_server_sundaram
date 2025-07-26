ce22bb8c8fba5d0cdd9a09c7c57f24f939287afd
SESA737865 <SESA737865@se.com>
2025-07-26 17:25:13 +0530

    adding track order function

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def hanlder(request:Request):
+    """Handles incoming requests to the root endpoint."""
+    #Fetch payload
+    input_payload = await request.json()
+
+    intent = input_payload['queryResult']['intent']['displayName']
+    parameters = input_payload['queryResult']['parameters']
+    output_context = input_payload['queryResult']['outputContexts']
+
+    if intent == "order.track":
+        return JSONResponse(
+            content={
+                "fulfillmentText": "Received request for tracking - Intent : {initent}",
+            }
+        )
