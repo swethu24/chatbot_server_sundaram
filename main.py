09bfadea9ed8eee0b57285169a99f7d82741fa5b
SESA737865 <SESA737865@se.com>
2025-07-26 17:54:10 +0530

    fix: update root handler to return a simple greeting

--- main.py
+++ main.py
@@ -5,13 +5,5 @@ from fastapi.responses import JSONResponse
 app = FastAPI()
 
 @app.get("/")
-async def handler(request:Request):
-    """Handles incoming requests to the root endpoint."""
-    input_payload = await request.json()
-    print(f"Received request: {input_payload}")
-    intent = input_payload['queryResult']['intent']['displayName']
-    parameters = input_payload['queryResult']['parameters']
-    output_context = input_payload['queryResult']['outputContexts']
-
-    if intent == "order.track":
-        return {f"Received request at backend {intent}"}
+async def root():
+    return {"Hello,World!"}
