04fdab323352e02469f74e7eaa613d6b05e46a3d
SESA737865 <SESA737865@se.com>
2025-07-26 18:30:51 +0530

    fix: update root handler to accept request parameter

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+import json
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
+async def root(request: Request):
+    print(f"Request received: {request.method} {request.url}")
     return {"Hello,World!"}
